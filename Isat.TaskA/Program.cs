b207a56ca082d2a172a908d48ba44a82bfd8b892
nomadedge <nomad.edge@gmail.com>
2020-02-18 19:01:46 +0300

    How mad was me to create previous solution?

--- Isat.TaskA/Program.cs
+++ Isat.TaskA/Program.cs
@@ -17,64 +17,39 @@ namespace Isat.TaskA
             var partsCount = int.Parse(numbers1[2]);
 
             var numbers2 = string2.Split(' ');
-            var Cs = new List<int>();
+            var objects = new List<int>(objectsCount);
             foreach (var number in numbers2)
             {
-                Cs.Add(int.Parse(number));
+                objects.Add(int.Parse(number));
             }
 
             //Sort objects by class
-            var objectsByClass = new List<List<int>>();
+            var objectsByClass = new List<List<int>>(classesCount);
             for (int i = 0; i < classesCount; i++)
             {
-                var iClass = new List<int>();
-                for (int j = 0; j < objectsCount; j++)
-                {
-                    if (Cs[j] == i + 1)
-                    {
-                        iClass.Add(j);
-                    }
-                }
-                objectsByClass.Add(iClass);
+                objectsByClass.Add(new List<int>());
+            }
+            for (int i = 0; i < objectsCount; i++)
+            {
+                objectsByClass[objects[i] - 1].Add(i);
             }
 
-            //Create two-dimensions list for objects sorted by part
-            var objectsByPart = new List<List<int>>();
+            //Create 2D list for objects sorted by part
+            var initialCapacity = objectsCount / partsCount + 1;
+            var objectsByPart = new List<List<int>>(partsCount);
             for (int i = 0; i < partsCount; i++)
             {
-                objectsByPart.Add(new List<int>());
+                objectsByPart.Add(new List<int>(initialCapacity));
             }
 
             //Sort objects by part
-            var lesserParts = new List<int>();
+            int j = -1;
             foreach (var iClass in objectsByClass)
             {
-                if (lesserParts.Count < iClass.Count)
+                for (int i = 0; i < iClass.Count; i++)
                 {
-                    for (int i = 0; i < lesserParts.Count; i++)
-                    {
-                        objectsByPart[lesserParts[i]].Add(iClass[i]);
-                    }
-                    int j = -1;
-                    for (int i = lesserParts.Count; i < iClass.Count; i++)
-                    {
-                        j = (j == partsCount - 1) ? 0 : j + 1;
-                        objectsByPart[j].Add(iClass[i]);
-                    }
-
-                    lesserParts.Clear();
-                    for (int i = j + 1; i < partsCount; i++)
-                    {
-                        lesserParts.Add(i);
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < iClass.Count; i++)
-                    {
-                        objectsByPart[lesserParts[i]].Add(iClass[i]);
-                    }
-                    lesserParts.RemoveRange(0, iClass.Count);
+                    j = (j == partsCount - 1) ? 0 : j + 1;
+                    objectsByPart[j].Add(iClass[i]);
                 }
             }
 
