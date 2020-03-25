1b9614c5fc95c688e6d4293dd88830f6640bae30
Mikhail <nomadedge@outlook.com>
2020-03-25 12:03:57 +0300

    Edit lab 5

--- Isat.Lab5/Program.py
+++ Isat.Lab5/Program.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import AdaBoostClassifier
 import os
 import pandas
+import numpy
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as pyplot
 
@@ -20,11 +21,11 @@ bestAccuracy, bestLearningRate, bestIteration = 0, 0, 0
 
 for i in range (0, 100):
     learningRate = 1e-3 * 1.1 ** i
-    model = AdaBoostClassifier(learning_rate = learningRate, n_estimators = 100)
-    model.fit(attributesTrain, targetValuesTrain)
+    classifier = AdaBoostClassifier(learning_rate = learningRate, n_estimators = 100)
+    classifier.fit(attributesTrain, targetValuesTrain)
 
-    targetValuePredictedFunctions = model.staged_predict(attributes)
-    scoreFunctions = model.staged_score(attributesTest, targetValueTest)
+    targetValuePredictedFunctions = classifier.staged_predict(attributes)
+    scoreFunctions = classifier.staged_score(attributesTest, targetValueTest)
 
     for j, item in enumerate(scoreFunctions, start = 0):
         if (item > bestAccuracy):
@@ -36,23 +37,49 @@ print(bestLearningRate)
 print(bestAccuracy)
 print(bestIteration)
 
-model = AdaBoostClassifier(learning_rate = bestLearningRate, n_estimators = 100)
-model.fit(attributesTrain, targetValuesTrain)
+classifier = AdaBoostClassifier(learning_rate = bestLearningRate, n_estimators = 100)
+classifier.fit(attributesTrain, targetValuesTrain)
 
-targetValuePredictedFunctions = model.staged_predict(attributes)
-scoreFunctions = model.staged_score(attributesTest, targetValueTest)
+targetValuePredictedFunctions = classifier.staged_predict(attributes)
+scoreFunctions = classifier.staged_score(attributesTest, targetValueTest)
+
+pyplot.scatter(attributes[:, 0], attributes[:, 1], c = colors(targetValues), cmap = pyplot.cm.Paired)
+
+axes = pyplot.gca()
+xLim = axes.get_xlim()
+yLim = axes.get_ylim()
+
+xs = numpy.linspace(xLim[0], xLim[1], 30)
+ys = numpy.linspace(yLim[0], yLim[1], 30)
+
+xCoords, yCoords = numpy.meshgrid(xs, ys)
+
+xyCoords = numpy.vstack([xCoords.ravel(), yCoords.ravel()]).T
+
+Z = classifier.staged_decision_function(xyCoords)
 
 iterationsCount = []
 accuracies = []
 
 os.mkdir(fileName)
 
-for i, item in enumerate(targetValuePredictedFunctions, start = 1):
+i = 1
+for item, z in zip(targetValuePredictedFunctions, Z):
+    pyplot.clf()
+
     pyplot.xlabel("Step number " + str(i))
     pyplot.scatter(attributes[:, 0], attributes[:, 1], c = colors(targetValues), cmap = pyplot.cm.Paired)
-    ax = pyplot.gca()
-    ax.scatter(attributes[:, 0], attributes[:, 1], facecolors = "none", edgecolors = colors(item), s = 100, linewidths = 1)
+
+    z = z.reshape(xCoords.shape)
+
+    axes = pyplot.gca()
+
+    axes.contour(xCoords, yCoords, z, colors = "black", levels = [-1, 0, 1], linestyles = ["--", "-", "--"])
+
+    axes.scatter(attributes[:, 0], attributes[:, 1], facecolors = "none", edgecolors = colors(item), s = 100, linewidths = 1)
     pyplot.savefig(fileName + "/" + "iteration" + str(i) + ".png")
+
+    i += 1
 
 for i, item in enumerate(scoreFunctions, start = 0):
     iterationsCount.append(i)
