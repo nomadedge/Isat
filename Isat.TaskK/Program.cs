a9810c1d7d5d7525ce0318151c5082babe338211
Mikhail <nomadedge@outlook.com>
2020-04-20 00:32:11 +0300

    task K

---
+++ Isat.TaskK/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Isat.TaskK
+{
+    class Solution
+    {
+        public int YCount { get; set; }
+        public int EntitiesCount { get; set; }
+        public Dictionary<int, List<int>> Entities { get; set; }
+        public List<Tuple<int, int>> EntitiesOriginal { get; set; }
+        public long AllDistance { get; set; }
+        public long InnerDistance { get; set; }
+        public long OuterDistance { get; set; }
+
+        public Solution(int yCount, int entitiesCount)
+        {
+            YCount = yCount;
+            EntitiesCount = entitiesCount;
+            Entities = new Dictionary<int, List<int>>(yCount);
+            EntitiesOriginal = new List<Tuple<int, int>>();
+
+            for (int i = 1; i < yCount + 1; i++)
+            {
+                Entities.Add(i, new List<int>());
+            }
+
+            ReadEntities();
+        }
+
+        private void ReadEntities()
+        {
+            long allDistance = 0;
+
+            for (int i = 0; i < EntitiesCount; i++)
+            {
+                var values = Console.ReadLine().Split(' ');
+                var x = Convert.ToInt32(values[0]);
+                var y = Convert.ToInt32(values[1]);
+                EntitiesOriginal.Add(new Tuple<int, int>(x, y));
+            }
+
+            EntitiesOriginal = EntitiesOriginal.OrderBy(eo => eo.Item1).ToList();
+
+            for (int i = 0; i < EntitiesCount; i++)
+            {
+                Entities[EntitiesOriginal[i].Item2].Add(EntitiesOriginal[i].Item1);
+
+                var difference = 2 * EntitiesCount - 2;
+                allDistance += EntitiesOriginal[i].Item1 * (4 * i - difference);
+            }
+
+            AllDistance = Math.Abs(allDistance);
+        }
+
+        public void Solve()
+        {
+            long innerDistance = 0;
+
+            for (int i = 1; i < YCount + 1; i++)
+            {
+                for (int j = 0; j < Entities[i].Count; j++)
+                {
+                    innerDistance += Entities[i][j] * (2 * j - Entities[i].Count + 1);
+                }
+            }
+
+            InnerDistance = innerDistance * 2;
+            OuterDistance = AllDistance - InnerDistance;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(InnerDistance);
+            Console.WriteLine(OuterDistance);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yCount = Convert.ToInt32(Console.ReadLine());
+            var entitiesCount = Convert.ToInt32(Console.ReadLine());
+
+            var solution = new Solution(yCount, entitiesCount);
+
+            solution.Solve();
+
+            solution.Print();
+        }
+    }
+}
--- Isat.TaskM/Program.cs
+++ Isat.TaskM/Program.cs
@@ -4,11 +4,101 @@ using System.Text;
 
 namespace Isat.TaskM
 {
+    class Solution
+    {
+        public int KX1 { get; set; }
+        public int KX2 { get; set; }
+        public int EntitiesCount { get; set; }
+        public Dictionary<int, int> X1Counts { get; set; }
+        public Dictionary<int, int> X2Counts { get; set; }
+        public List<Dictionary<int, int>> PairCounts { get; set; }
+        public double Hi2 { get; set; }
+
+        public Solution(int kX1, int kX2, int entitiesCount)
+        {
+            KX1 = kX1;
+            KX2 = kX2;
+            EntitiesCount = entitiesCount;
+
+            X1Counts = new Dictionary<int, int>(KX1);
+            X2Counts = new Dictionary<int, int>(KX2);
+            PairCounts = new List<Dictionary<int, int>>(KX1);
+            for (int i = 0; i < KX1; i++)
+            {
+                PairCounts.Add(new Dictionary<int, int>());
+            }
+
+            ReadEntities();
+        }
+
+        private void ReadEntities()
+        {
+            for (int i = 0; i < EntitiesCount; i++)
+            {
+                var values = Console.ReadLine().Split(' ');
+                var x1 = Convert.ToInt32(values[0]) - 1;
+                var x2 = Convert.ToInt32(values[1]) - 1;
+                
+                if (X1Counts.ContainsKey(x1))
+                {
+                    X1Counts[x1] += 1;
+                }
+                else
+                {
+                    X1Counts.Add(x1, 1);
+                }
+
+                if (X2Counts.ContainsKey(x2))
+                {
+                    X2Counts[x2] += 1;
+                }
+                else
+                {
+                    X2Counts.Add(x2, 1);
+                }
+
+                if (PairCounts[x1].ContainsKey(x2))
+                {
+                    PairCounts[x1][x2] += 1;
+                }
+                else
+                {
+                    PairCounts[x1].Add(x2, 1);
+                }
+            }
+        }
+
+        public void Solve()
+        {
+            double hi2 = EntitiesCount;
+
+            for (int i = 0; i < KX1; i++)
+            {
+                foreach (var pairCount in PairCounts[i])
+                {
+                    var value = X1Counts[i] * X2Counts[pairCount.Key] / (double)EntitiesCount;
+                    hi2 += Math.Pow(pairCount.Value - value, 2) / value - value;
+                }
+            }
+
+            Hi2 = hi2;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var values = Console.ReadLine().Split(' ');
+            var kX1 = Convert.ToInt32(values[0]);
+            var kX2 = Convert.ToInt32(values[1]);
+            var entitiesCount = Convert.ToInt32(Console.ReadLine());
 
+            var solution = new Solution(kX1, kX2, entitiesCount);
+
+            solution.Solve();
+
+            Console.WriteLine(solution.Hi2);
         }
     }
 }
