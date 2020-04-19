3e9909303b2776d7c9ea7f0758471f3b4ad6b33f
Mikhail <nomadedge@outlook.com>
2020-04-19 12:58:14 +0300

    Working on task D

--- Isat.TaskD/Program.cs
+++ Isat.TaskD/Program.cs
@@ -5,12 +5,14 @@ namespace Isat.TaskD
 {
     class Entity
     {
-        public List<int> Features { get; set; }
+        public List<double> Features { get; set; }
+        public List<double> NormalizedFeatures { get; set; }
         public int TargetValue { get; set; }
 
         public Entity(int featuresCount)
         {
-            Features = new List<int>(featuresCount);
+            Features = new List<double>(featuresCount);
+            NormalizedFeatures = new List<double>(featuresCount);
         }
     }
 
@@ -43,6 +45,90 @@ namespace Isat.TaskD
                 entity.TargetValue = Convert.ToInt32(featureStrings[FeaturesCount]);
                 Entities.Add(entity);
             }
+
+            Normalize();
+        }
+
+        public void Solve(int epochsCount, double learningRate)
+        {
+            //Coefficients.AddRange(new double[FeaturesCount + 1]);
+            var random = new Random();
+            for (int i = 0; i < FeaturesCount + 1; i++)
+            {
+                Coefficients.Add(random.NextDouble());
+            }
+
+            for (int epochNumber = 1; epochNumber < epochsCount + 1; epochNumber++)
+            {
+                var sumError = 0d;
+                foreach (var entity in Entities)
+                {
+                    var predictedValue = Predict(entity.NormalizedFeatures);
+                    var error = predictedValue - entity.TargetValue;
+                    sumError += Math.Pow(error, 2);
+                    for (int i = 0; i < FeaturesCount; i++)
+                    {
+                        Coefficients[i] -= learningRate * error * entity.NormalizedFeatures[i];
+                    }
+                    Coefficients[FeaturesCount] -= learningRate * error;
+                }
+
+                Console.WriteLine($"Epoch: {epochNumber}\tError = {sumError}");
+                foreach (var coefficient in Coefficients)
+                {
+                    Console.Write($"{coefficient} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        public double Predict(List<double> features)
+        {
+            if (features.Count != FeaturesCount)
+            {
+                return 0;
+            }
+
+            var predictedValue = 0d;
+            for (int i = 0; i < FeaturesCount; i++)
+            {
+                predictedValue += features[i] * Coefficients[i];
+            }
+            predictedValue += Coefficients[FeaturesCount];
+
+            return predictedValue;
+        }
+
+        private void Normalize()
+        {
+            for (int i = 0; i < FeaturesCount; i++)
+            {
+                var average = 0d;
+                var rmse = 0d;
+                for (int j = 0; j < EntitiesCount; j++)
+                {
+                    average += Entities[j].Features[i];
+                }
+                average /= EntitiesCount;
+                for (int j = 0; j < EntitiesCount; j++)
+                {
+                    rmse += Math.Pow(Entities[j].Features[i] - average, 2);
+                }
+                rmse = Math.Sqrt(rmse / Entities.Count);
+                for (int j = 0; j < Entities.Count; j++)
+                {
+                    Entities[j].NormalizedFeatures.Add((Entities[j].Features[i] - average) / rmse);
+                }
+            }
+        }
+
+        public void Print()
+        {
+            foreach (var coefficient in Coefficients)
+            {
+                Console.WriteLine(coefficient);
+            }
         }
     }
 
@@ -50,7 +136,7 @@ namespace Isat.TaskD
     {
         static void Main(string[] args)
         {
-            var countStrings = Console.ReadLine();
+            var countStrings = Console.ReadLine().Split(' ');
             var entitiesCount = Convert.ToInt32(countStrings[0]);
             var featuresCount = Convert.ToInt32(countStrings[1]);
             var entityStrings = new List<string>();
@@ -61,7 +147,9 @@ namespace Isat.TaskD
 
             var solution = new Solution(entitiesCount, featuresCount, entityStrings);
 
+            solution.Solve(1000, 0.01);
 
+            solution.Print();
         }
     }
 }
